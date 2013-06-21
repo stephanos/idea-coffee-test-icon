524816dd8a7d2a57e44033f6eb30d045211b6d96
stephanos <github@stephanbehnke.de>
2013-06-21 16:33:55 +0200

    updated coffeescript test file check

--- src/com/loops101/icons/CoffeescriptIconProvider.java
+++ src/com/loops101/icons/CoffeescriptIconProvider.java
@@ -10,24 +10,17 @@ import javax.swing.*;
 
 public class CoffeescriptIconProvider extends IconProvider {
 
-    //private static final String ROUTE_URL = "/net/jangaroo/EXML-logo-16x16.png";
-    //private static final String CTRL_URL = "/net/jangaroo/EXML-logo-16x16.png";
-    //private static final String VIEW_URL = "/net/jangaroo/EXML-logo-16x16.png";
-    private static final String TEST_URL = "/com/loops101/icons/coffeescript-test.png";
+    private static final String TEST_ICON_URL = "/com/loops101/icons/coffeescript-test.png";
 
     public Icon getIcon(@NotNull PsiElement psiElement, int flags) {
 
         PsiFile containingFile = psiElement.getContainingFile();
 
         if (containingFile != null) {
-            if (containingFile.getName().endsWith(".route")) {
-                return null;
-            } else if (containingFile.getName().endsWith(".ctrl")) {
-                return null;
-            } else if (containingFile.getName().endsWith(".view")) {
-                return null;
+            if (containingFile.getName().endsWith("-test.coffee")) {
+                return IconLoader.getIcon(TEST_ICON_URL);
             } else if (containingFile.getName().endsWith(".test")) {
-                return IconLoader.getIcon(TEST_URL);
+                return IconLoader.getIcon(TEST_ICON_URL);
             }
         }
 
